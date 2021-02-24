448b6990825906f1755f33a25f20626508aa0258
Alper Dursun <dursunalper1@gmail.com>
2021-02-24 19:55:05 +0300

    add 404 page

--- src/core/Route.php
+++ src/core/Route.php
@@ -5,8 +5,14 @@ namespace SpeedWeb\core;
 class Route
 {
 
+    public static $pages = [];
+    public static $page404 = null;
+
     public static function run(string $url, $callback, string $method = "get"): void
     {
+        if(!in_array($url, self::$pages)){
+            array_push(self::$pages, $url);
+        }
         $method = explode("|", strtoupper($method));
         if(in_array($_SERVER["REQUEST_METHOD"], $method)){
             $patterns = [
@@ -15,17 +21,31 @@ class Route
             ];
             $url = str_replace(array_keys($patterns), array_values($patterns), $url);
             $request_uri = $_SERVER["REQUEST_URI"];
-            $explode = explode("/", $request_uri);
-            if(preg_match('@^' . $url . '$@', $request_uri, $parameters)){
-                unset($parameters[0]);
-                if(is_callable($callback)){
-                    call_user_func_array($callback, $parameters);
+            if(in_array($request_uri, self::$pages)){
+                $explode = explode("/", $request_uri);
+                if(preg_match('@^' . $url . '$@', $request_uri, $parameters)){
+                    unset($parameters[0]);
+                    if(is_callable($callback)){
+                        call_user_func_array($callback, $parameters);
+                    }else{
+                        [$controllerName, $methodName] = explode('@', $callback);
+                        $controllerName = '\SpeedWeb\controllers\\' . $controllerName;
+                        $controller = new $controllerName();
+                        call_user_func_array([$controllerName, $methodName], $parameters);
+                    }
                 }
-                [$controllerName, $methodName] = explode('@', $callback);
-                $controllerName = '\SpeedWeb\controllers\\' . $controllerName;
-                $controller = new $controllerName();
-                call_user_func_array([$controllerName, $methodName], $parameters);
+            }else{
+                if(self::$page404 == null){
+                    echo "Page not found";
+                }else{
+                    header('Location: ' . self::$page404);
+                }
             }
         }
+    }
+
+    public static function set404Page(string $name)
+    {
+        self::$page404 = $name;
     }
 }
